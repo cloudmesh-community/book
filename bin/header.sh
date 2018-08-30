24f296d47392a5dc1dca535ea176bd766b1c013e
Gregor von Laszewski <laszewski@gmail.com>
2018-08-30 08:02:37 -0400

    experiment with header levels

--- bin/header.sh
+++ bin/header.sh
@@ -1,5 +1,8 @@
-#! /bin/sh
+#! /bin/sh -x
+echo "File:" $1
+echo "Level:" $2
+rm -f /tmp/convert.md
 pandoc $1 --base-header-level=$2 -o /tmp/convert.md
-cp /tmp/convert.md $1
+cp /tmp/convert.md dest/chapters/$1
 
 
