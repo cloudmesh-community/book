c72b1e4e77d16efc0d8d6d12ac09be1c3359f618
Gregor von Laszewski <laszewski@gmail.com>
2018-09-12 18:36:49 -0400

    use the old header

--- bin/header.py
+++ bin/header.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python
+#!/usr/bin/env python
+from __future__ import print_function
+import sys
+import os
+
+data = {
+    "filename": sys.argv[1],
+    "level": int(sys.argv[2])
+    
+
+}
+
+# print ("Adjusting headers {filename} -> {level}".format(**data))
+# print (79 * "=")
+
+os.system("rm -f /tmp/convert.md")
+os.system("pandoc {filename} --base-header-level={level} -o /tmp/convert.md".format(**data))
+
+
+# os.system("head  /tmp/convert.md")
+
+os.system ("cp /tmp/convert.md {filename}".format(**data))
+os.system("echo >> {filename}".format(**data))
+
+
+
+
+"""
+
+NEW CODE THAT WORKD KIND OF BUT NOT REALY 
+
+
 from __future__ import print_function
 import sys
 import os
@@ -35,3 +67,4 @@ print(line)
 to_file = open("{filename}".format(**data),mode="w")
 to_file.write(line)
 shutil.copyfileobj(convert, to_file)
+"""
