faeacee4e2f89b79cc99b6ec57626b044b3b24c3
Gregor von Laszewski <laszewski@gmail.com>
2018-09-22 16:23:28 -0400

    cleanup for container review

---
+++ chapters/cloud/images/vm.mm
@@ -0,0 +1,12 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1537451866836" ID="ID_1615833377" MODIFIED="1537452059420" TEXT="Virtualization">
+<node CREATED="1537451947503" ID="ID_1432803165" MODIFIED="1537451957198" POSITION="right" TEXT="Virtual Machine">
+<node CREATED="1537451961891" ID="ID_1231341183" MODIFIED="1537451983535" TEXT="System Virtual Machines">
+<node CREATED="1537452002494" ID="ID_187814526" MODIFIED="1537452011903" TEXT="Hosted Virtualization"/>
+<node CREATED="1537452013912" ID="ID_18707247" MODIFIED="1537452025538" TEXT="Bare-metal Virtualization"/>
+</node>
+<node CREATED="1537451987292" ID="ID_73650244" MODIFIED="1537451996220" TEXT="Process Virtual Machines"/>
+</node>
+</node>
+</map>
