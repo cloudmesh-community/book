cf02a33b5285e465b13133315b87468ca66e574a
Gregor von Laszewski <laszewski@gmail.com>
2019-01-20 10:04:46 -0500

    update ubuntu script

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -7,20 +7,26 @@ sudo update-alternatives --config python3
 sudo apt-get install haskell-platform -y
 sudo wget -qO- https://get.haskellstack.org/ | sh
 
-sudo cabal update
-sudo stack setup
-# add this also to your .bashrc file
-PATH="$HOME/root/.local/bin:$HOME/.cabal/bin:${PATH}"
+export PATH="$HOME/root/.local/bin:$HOME/.cabal/bin:${PATH}"
 
-SOFTWARE=pandoc-2.5
+sudo cabal update
+# for vagrant
+sudo chown $USER .cabal
+sudo chgrp $USER .cabal
+stack setup
+stack solver
+# add this also to your .bashrc file
+
+export SOFTWARE=pandoc-2.5
 cd $HOME
 wget http://hackage.haskell.org/package/${SOFTWARE}/${SOFTWARE}.tar.gz
 tar xvzf ${SOFTWARE}.tar.gz
 cd ${SOFTWARE}
-RUN stack init
-RUN stack install
+#stack solver
+#stack init
+stack install
 
-SOFTWARE=pandoc-citeproc-0.15.0.1
+export SOFTWARE=pandoc-citeproc-0.15.0.1
 cd $HOME
 wget http://hackage.haskell.org/package/${SOFTWARE}/${SOFTWARE}.tar.gz
 tar xvzf ${SOFTWARE}.tar.gz
@@ -28,7 +34,7 @@ cd ${SOFTWARE}
 stack init
 stack install
 
-SOFTWARE=pandoc-crossref-0.3.4.0
+export SOFTWARE=pandoc-crossref-0.3.4.0
 cd $HOME
 wget http://hackage.haskell.org/package/${SOFTWARE}/${SOFTWARE}.tar.gz
 tar xvzf ${SOFTWARE}.tar.gz
