9afc8c3e6e72c0de0402bb6d66d1a7eebea607ab
Gregor von Laszewski <laszewski@gmail.com>
2019-01-20 11:13:42 -0500

    update ubuntu script

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -5,11 +5,13 @@ sudo apt-get install -y python3.7-dev
 sudo apt-get install -y python3.7-venv
 
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 10
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 10
 sudo update-alternatives --config python3
 
 sudo apt-get install haskell-platform -y
 sudo wget -qO- https://get.haskellstack.org/ | sh
 
+# add this also to your .bashrc file
 export PATH="$HOME/root/.local/bin:$HOME/.cabal/bin:${PATH}"
 
 sudo cabal update
@@ -17,8 +19,22 @@ sudo cabal update
 sudo chown $USER .cabal
 sudo chgrp $USER .cabal
 stack setup
-stack solver
-# add this also to your .bashrc file
+
+
+#
+# install pandoc via cabal
+#
+cabal install cabal-install
+cabal update
+cabal install pandoc
+cabal install pandoc-citeproc
+cabal install pandoc-crossref
+
+exit
+
+
+
+
 
 export SOFTWARE=pandoc-2.5
 cd $HOME
@@ -26,8 +42,10 @@ wget http://hackage.haskell.org/package/${SOFTWARE}/${SOFTWARE}.tar.gz
 tar xvzf ${SOFTWARE}.tar.gz
 cd ${SOFTWARE}
 #stack solver
-#stack init
+stack init
+stack build
 stack install
+
 
 export SOFTWARE=pandoc-citeproc-0.15.0.1
 cd $HOME
