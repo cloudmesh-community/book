9b2aa163a05106d5001a1d4a732bc33682481d46
Gregor von Laszewski <laszewski@gmail.com>
2019-08-31 18:29:02 -0400

    update python book

--- bin/git.py
+++ bin/git.py
@@ -7,9 +7,50 @@ from pathlib import Path
 import time
 
 repos = [
-["fa19-516-156", "Manikandan Nagarajan", "ManiNagarajan20"],
-["fa19-516-157", "Chenxu Wang",	"wang542"],
-["fa19-516-158", "Daivik Dayanand", "Daivik1997"]
+    ["fa19-523-180", "Jonathon Grant","jongrant010"],
+    ["fa19-523-181", "Max Falkenstein","maxf7399"],
+    ["fa19-523-182", "Zak Siddiqui","zaksidd1"],
+    ["fa19-523-183", "Brent Creech","bcreech40"],
+    ["fa19-523-184", "Michael Floreak","mfloreak"],
+    ["fa19-523-185", "Die Hu","syhu"],
+    ["fa19-523-186", "Soowon Park","parksoow"],
+    ["fa19-523-187", "Chris Fang","zihfang"],
+    ["fa19-523-188", "Shivani Katukota","katukota"],
+    ["fa19-523-189", "Huizhou Wang","aiai949"],
+    ["fa19-523-190", "Skyler Konger","skonger"],
+    ["fa19-523-191", "Yiyu Tao","oliviatao22"],
+    ["fa19-523-192", "Jihoon Kim","Kim926"],
+    ["fa19-523-193", "Lin-Fei Sung","lfsung1997"],
+    ["fa19-523-194", "Ashley Minton","ashmmint"],
+    ["fa19-523-195", "Kang Jie Gan","kanggan"],
+    ["fa19-523-196", "Xinzhuo Zhang","zhanxinz"],
+    ["fa19-523-198", "Dominic Matthys","dojomatt"],
+    ["fa19-523-199", "Lakshya Gupta","lakshyagupta96"],
+    ["fa19-523-200", "Naimesh Chaudhari","nchaudh03"],
+    ["fa19-523-201", "Ross Bohlander","rbohland"],
+    ["fa19-523-202", "Limeng Liu","liulim-Liu"],
+    ["fa19-523-203", "Jisang Yoo","jisayoo"],
+    ["fa19-523-204", "Andrew Dingman","acdingman"],
+    ["fa19-523-205", "Senthil Palani","psendil"],
+    ["fa19-523-206", "Lenin Arivukadal","wowlenin"],
+    ["fa19-523-207", "Nihir Chadderwala","nihirc"],
+
+#["fa19-516-159", "Austin Zebrowski", "azebrowski"],
+#"fa19-516-160","Shreyans Jain", "toshreyansjain"],
+#["fa19-516-161","Jim Nelson", "JENelson77"],
+#["fa19-516-162","Shivani Katukota, ", "katukota"],
+#["fa19-516-163","John Hoerr", "jhoerr"],
+#["fa19-516-164","Siddhesh Mirjankar", "smirjank"],
+#["fa19-516-165","Zhi Wang", "zwang89"],
+#["fa19-516-166","Brian Funk", "brianfunk3"],
+#["fa19-516-167","William Screen", "william-screen"],
+#["fa19-516-168","Deepak Deopura", "deepakdeopura"],
+#["fa19-516-169","Harshawardhan Pandit", "hppanditims"]
+#["fa19-516-170","Yanting Wan", "Yanting Wan"],
+
+#["fa19-516-156", "Manikandan Nagarajan", "ManiNagarajan20"],
+#["fa19-516-157", "Chenxu Wang",	"wang542"],
+#["fa19-516-158", "Daivik Dayanand", "Daivik1997"]
 #    ["fa19-516-000", "Gregor von Laszewski", "laszewsk"]
 ]
 
@@ -67,7 +108,7 @@ for r in repos:
           firstname: "{firstname}"
           lastname: "{lastname}"
           hid: "{name}"
-          community: "e516"
+          community: "523"
           semester: "fa19"
         """).strip()
     print(readme)
