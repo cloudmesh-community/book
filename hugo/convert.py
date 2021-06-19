48faa63f89eb2220a1ae2e74726686eb9391edfc
Gregor von Laszewski <laszewski@gmail.com>
2021-06-19 10:58:17 -0400

    add hugo converter

--- hugo/convert.py
+++ hugo/convert.py
@@ -1,8 +1,12 @@
 import click
 from textwrap import dedent
 import os
+from cloudmesh.common.util import path_expand
+from pathlib import Path
 
-
+# ${CONVERT} --file=${BASE}/google-colab/python-google-colab.md --d=${DEST}/python/first-steps
+#https://github.com/cloudmesh-community/book/raw/main/chapters/prg/python/google-colab/images/colab-3.png
+# https://github.com/cloudmesh-community/book/raw/main/chapters/prg/python/google-colab/images/colab-3.png
 
 @click.command()
 @click.option('--file', help='The file name to convert')
@@ -11,17 +15,68 @@ import os
 @click.option('--d', help='The directory')
 @click.option('--w', help='The weight')
 @click.option('--index', help='The weight')
-def convert(file, tags, author, d, w, index):
+@click.option('--url', help='file url')
+@click.option('--crossref', help='apply crossref')
+def convert(file, tags, author, d, w, index, url, crossref):
     """
     Convert the file to use yaml front matter
     """
+
+
+    if crossref is not None:
+        print ("FFFF", file)
+        name = os.path.basename(file)
+        print ("NNNN", name)
+        tmp = f"/tmp/{name}"
+
+        os.system(f"cp {file} {tmp}")
+        command = f"pandoc --wrap=preserve -M crossrefYaml=prefix.yaml --filter pandoc-crossref --citeproc -t markdown -o {tmp}-tmp {tmp} "
+        print("PANDOC", command)
+        os.system(command)
+        os.system(f"mv {tmp}-tmp {tmp}")
+        file = tmp
+
+        # add captions
+        result = []
+        with open(tmp, "r") as f:
+            lines = f.read()
+        lines = lines.split("\n")
+        for line in lines:
+            if "![" in line:
+                caption = line.strip().split("]")[0][2:]
+                line = line.split("{#")[0]  # remove label
+                line = f"{line}\n\n{caption}"  # add caption from alttext
+            result.append(line)
+        with open(tmp, "w") as f:
+            f.write("\n".join(result))
+
+    print (79* "=")
+
+    print("Tags:  ", tags)
+    print("Author:", author)
+    print("Dir:   ", Path(d).resolve())
+    if file:
+        print("File:  ", Path(file).resolve())
+    if url:
+        print("URL    ", url)
+        images = os.path.dirname(url)
+        print("Base   ", images)
+
+    print()
+
 
     if index:
         result  = dedent(f"""
         ---
         title: "{index}"
         linkTitle: "{index}"
+        weight: {w}
+        authors: "{author}"
+        github_url: "{url}"
         ---
+        
+        {author}
+        
         """).strip().splitlines()
         file = "_index.md"
 
@@ -53,17 +108,31 @@ def convert(file, tags, author, d, w, index):
             matter.append(f"tags: {tags}")
 
         if author is not None:
-            matter.append(f'author: "{author}"')
+            matter.append(f'authors: "{author}"')
         if w is not None:
             matter.append(f"weight: {w}")
+        if url is not None:
+            matter.append(f'github_url: "{url}"')
 
         result.append("---")
         result.append("\n".join(matter))
-        result.append("---")
-
+        result.append("---\n")
+        result.append(f"{author}\n\n")
 
         for line in content[1:]:
             line = line.strip("\n")
+
+            if "![" in line:
+                line = line.replace("{width=20%}", "")
+                line = line.replace("(images/", f"({images}/images/")
+                print ("image", line)
+
+                # Remove {
+                if "{#" in line:
+                    line, label = line.split("{#")
+                # Make alttext caption
+
+
             result.append(line)
 
     if d is None:
@@ -71,11 +140,16 @@ def convert(file, tags, author, d, w, index):
     else:
         name = os.path.basename(file)
         filename = f"{d}/{name}"
-        print ("NAM:", filename)
-        
+
+        directory = os.path.dirname(filename)
+
+        print ("CONVERT:", filename, "to", directory)
+
+        os.makedirs(directory, exist_ok=True)
+
         with open(filename, "w") as output:
             output.write("\n".join(result))
-        
-            
+
+
 if __name__ == '__main__':
     convert()
---
+++ hugo/example.md
@@ -0,0 +1,9 @@
+# headline
+
+
+![Histogram](images/secchi/output_9_1.png){#fig:secchihistogram}
+
+@fig:secchihistogram: The caption
+
+See @fig:secchihistogram
+
